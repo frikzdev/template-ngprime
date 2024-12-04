2c80cbfd018d858915dadbbb05a789cb21a6e5e5
Luis Eduardo Mendez Tafolla <luismendeztaf3@gmail.com>
2024-12-04 17:26:13 -0600

    Se configuro primeNG y primeFlex

--- templatePrime/src/app/app.module.ts
+++ templatePrime/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {StyleClassModule} from 'primeng/styleclass';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    StyleClassModule
   ],
   providers: [],
   bootstrap: [AppComponent]
